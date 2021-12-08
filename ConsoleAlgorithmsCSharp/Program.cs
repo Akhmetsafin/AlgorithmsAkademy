a69d289f1ed182261a1397387fb8b728257c128f
Artur <artur071982@gmail.com>
2021-12-08 18:11:44 +0500

    final code

--- ConsoleAlgorithmsCSharp/Program.cs
+++ ConsoleAlgorithmsCSharp/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleAlgorithmsCSharp
     {
         static void Main(string[] args)
         {
-            
+            Simple_number simple_Number = new Simple_number();
+            Console.WriteLine(simple_Number.welcom);
+            simple_Number.Core();
         }
     }
 }

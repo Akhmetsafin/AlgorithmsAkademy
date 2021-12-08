42185ece711789d186e74fd4bd5824a663611b8e
Artur <artur071982@gmail.com>
2021-12-08 21:45:02 +0500

    final

--- FibonachiNum/FibonachiBody.cs
+++ FibonachiNum/FibonachiBody.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FibonachiNum
 {
@@ -11,27 +7,28 @@ namespace FibonachiNum
         /// <summary>
         /// Вывод на косоль вычислений метода FibonachiRecurtion 
         /// </summary>
-        static void Run()
+        static void Run(string textNum)
         {
-            Console.WriteLine("Введите число:");
-            string textNum = Console.ReadLine();
+            Console.WriteLine("Число Фибоначи с рекурсией");
+
             if (int.TryParse(textNum, out int numberInter))
 
             {
 
-                for (int i = 0; i < numberInter; i++)
+                for (int i = 0; i < numberInter+1; i++)
                 {
-                    if (i < numberInter - 1)
+                    if (i < numberInter)
                         Console.Write($"{FibonachiRecurtion(i)} | ");
                     else
                         Console.WriteLine($"{FibonachiRecurtion(i)} | ");
                 }
-                Console.WriteLine("продолжить нажмите любую клавишу, выйти нажмите ESC");
-                
+
+
             }
             else
             {
                 Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+
             }
 
         }
@@ -44,18 +41,68 @@ namespace FibonachiNum
         {
             if (f == 0) return 0;
             if (f == 1) return 1;
-            return FibonachiRecurtion(f - 1) + FibonachiRecurtion(f - 2);
-            
+            return FibonachiRecurtion(f-1 ) + FibonachiRecurtion(f - 2);
+
 
         }
+        /// <summary>
+        /// Фибоначи без рекурсии
+        /// </summary>
+        /// <param name="textNumnoRecur"></param>
+        /// <returns></returns>
+        static void Fibonachi(string textNumnoRecur)
+        {
+            int result = 0;
+            Console.WriteLine("Число Фибоначи без рекурсии");
+            if (int.TryParse(textNumnoRecur, out int numberInter))
+               
+            {
+
+                int b = 1;
+                int tmp;
+
+                for (int i = 0; i < numberInter; i++)
+
+                {
+                    tmp = result;
+                    result = b;
+                    b += tmp;
+
+                    if (i < numberInter-2)
+                    {
+                        if (i == 0) Console.Write($"{(0)} | ");
+                        if (i == 1) Console.Write($"{(1)} | ");
+                        Console.Write($"{(b)} | ");
+                    }
+                    if (i== numberInter-2)
+                        Console.WriteLine($"{(b)} | ");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+
+            }
+
+
+        }
+        /*  static int FibonachiNoRec(int n)
+          Console.WriteLine("Введите число:");
+              string textNum = Console.ReadLine();
+              if (int.TryParse(textNum, out int numberInter))
+
+              {*/
+
         public void Core()
         {
             do
             {
-                //Console.WriteLine("Введите число: ");
-                Run();
-
-
+                Console.WriteLine("Введите число:");
+                string textNum = Console.ReadLine();
+                Run(textNum);
+                Fibonachi(textNum);
+                Console.WriteLine("продолжить нажмите любую клавишу, выйти нажмите ESC");
             }
             while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
